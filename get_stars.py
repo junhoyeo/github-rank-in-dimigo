ca929d362d396a3fdfe8ea1c59f70e5436c2ec69
JunhoYeo <hanaro0704@gmail.com>
2020-07-21 19:56:43 +0900

    feat: implement get_stars

--- get_stars.py
+++ get_stars.py
@@ -1,18 +1,23 @@
-import pandas
-import pygithubutils.repo_stats as github_repo_stats
+import requests
+from bs4 import BeautifulSoup
+
+def _count_stars_from_url(url: str) -> int:
+  response = requests.get(url)
+  soup = BeautifulSoup(response.text, 'html.parser')
+  star_counts = [
+    int(anchor.text.strip())
+    for anchor in soup.select('a.muted-link.mr-3')
+    if 'stargazers' in anchor.attrs['href']
+  ]
+  print(star_counts)
+  next_button = soup.select_one('a.btn.btn-outline.BtnGroup-item:last-child')
+  if next_button and 'Next' in next_button.text:
+    next_url = next_button['href']
+    return sum(star_counts) + _count_stars_from_url(next_url)
+  return sum(star_counts)
 
 def get_stars(user_id: str) -> int:
-  dat = pandas.DataFrame(None)
-
-  counts = github_repo_stats.repo_prober(user_id, None, None, True)[0]
-  d = pandas.DataFrame([c[1:] for c in counts], index=[c[0]
-                        for c in counts], columns=['stars'])
-  dat = pandas.concat((dat, d))
-
-  datnz = dat[~(dat == 0).all(axis=1)].drop_duplicates()
-  pandas.set_option('display.max_rows', 500)
-
-  return datnz['stars'].sum()
+  return _count_stars_from_url(f'https://github.com/{user_id}?tab=repositories')
 
 if __name__ == '__main__':
   print(get_stars('junhoyeo'))
