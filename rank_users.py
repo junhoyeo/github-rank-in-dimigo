393244070b54b1c53d12bb0927ce01c0c5046d1c
JunhoYeo <hanaro0704@gmail.com>
2020-07-21 21:45:27 +0900

    feat: render ranked to template

--- rank_users.py
+++
@@ -1,7 +0,0 @@
-import database
-
-users = database.query_all_users()
-print(users)
-
-ranked = sorted(users, key=lambda user: user['stars'], reverse=True)
-print(*[user['name'] for user in ranked], sep='\n')
---
+++ render_users_with_rank.py
@@ -0,0 +1,31 @@
+import database
+
+import datetime
+from jinja2 import Environment, FileSystemLoader
+
+file_loader = FileSystemLoader('.')
+env = Environment(loader=file_loader)
+
+template = env.get_template('template.html')
+
+users = []
+for user in database.query_all_users():
+  timestamp = user['updated_at']
+  user['updated_at'] = str(datetime.datetime.fromtimestamp(timestamp))
+
+  user_bio = user['bio']
+  user['bio'] = user_bio if user_bio else '소개 없음'
+
+  if user['stars'] > 0:
+    users.append(user)
+
+ranked = sorted(
+  users,
+  key=lambda user: (user['stars'], user['followers'], user['public_repos']),
+  reverse=True
+)
+
+output = template.render(ranked_users=ranked)
+
+with open('./index.html', 'w') as file_pointer:
+  file_pointer.write(output)
