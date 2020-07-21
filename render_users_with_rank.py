2151c30d1f7196f1511742e5363e9f3262106617
JunhoYeo <hanaro0704@gmail.com>
2020-07-22 01:15:47 +0900

    fix: same stars, same rank

--- render_users_with_rank.py
+++ render_users_with_rank.py
@@ -8,24 +8,35 @@ env = Environment(loader=file_loader)
 
 template = env.get_template('template.html')
 
-users = []
-for user in database.query_all_users():
+ranked = sorted(
+  database.query_all_users(),
+  key=lambda user: (user['stars'], user['followers'], user['public_repos']),
+  reverse=True
+)
+
+ranked_users = []
+current_rank = 0
+previous_stars = None
+
+for user in ranked:
+  user_stars = user['stars']
+  if user_stars < 1:
+    continue
+
   timestamp = user['updated_at']
   user['updated_at'] = str(datetime.datetime.fromtimestamp(timestamp))
 
   user_bio = user['bio']
   user['bio'] = user_bio if user_bio else '소개 없음'
 
-  if user['stars'] > 0:
-    users.append(user)
+  if user_stars != previous_stars:
+    current_rank += 1
+  user['rank'] = current_rank
+  previous_stars = user_stars
 
-ranked = sorted(
-  users,
-  key=lambda user: (user['stars'], user['followers'], user['public_repos']),
-  reverse=True
-)
+  ranked_users.append(user)
 
-output = template.render(ranked_users=ranked)
+output = template.render(ranked_users=ranked_users)
 
 with open('./build/index.html', 'w') as file_pointer:
   file_pointer.write(output)
