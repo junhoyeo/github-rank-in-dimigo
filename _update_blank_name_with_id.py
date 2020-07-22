0a85d90106500e610c938a437885ef20ed9ce795
JunhoYeo <hanaro0704@gmail.com>
2020-07-22 17:32:56 +0900

    chore: clean legacy structure

--- _update_blank_name_with_id.py
+++
@@ -1,9 +0,0 @@
-import database
-
-if __name__ == '__main__':
-  users = database.query_all_users()
-
-  for user in users:
-    if not user['name']:
-      user['name'] = user['id']
-      print('update', database.update_user(user))
--- deploy.py
+++
@@ -1,3 +0,0 @@
-import os
-os.system('github-pages-publish -m "Update" -V . ./build')
-os.system('git push origin gh-pages')
