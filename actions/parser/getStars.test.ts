c105c5a4b17630b7da9bc01ee3b8a3309350dad5
JunhoYeo <hanaro0704@gmail.com>
2020-07-22 18:06:35 +0900

    feat(actions/parser): implement getStars

---
+++ actions/parser/getStars.test.ts
@@ -0,0 +1,10 @@
+import getStars from './getStars';
+
+const SECONDS = 1000;
+
+describe('Get stars', () => {
+  test('Get stars from userID', async () => {
+    const stars: number = await getStars('junhoyeo');
+    expect(stars).toBeGreaterThan(300);
+  }, 30 * SECONDS);
+})
---
+++ actions/parser/getStars.ts
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import cheerio from 'cheerio';
+
+function getSumOfArray(arrayOfNumbers: number[]): number {
+  return arrayOfNumbers.reduce((a, b) => a + b, 0);
+}
+
+async function _countStarsFromURL(url: string): Promise<number> {
+  const { data: html } = await axios.get(url);
+  const document = cheerio.load(html);
+  const starCounts = document('a.muted-link.mr-3')
+    .toArray()
+    .flatMap((anchorReference) => {
+      const anchor = cheerio(anchorReference);
+      const href = anchor.attr('href');
+      if (href && href.includes('stargazers')) {
+        return [
+          Number(anchor.text().replace(/,+/g, '').trim())
+        ];
+      }
+      return [];
+    });
+  console.log(starCounts);
+  const currentCounts = getSumOfArray(starCounts);
+
+  const nextButton = document('a.btn.btn-outline.BtnGroup-item:last-child').first();
+  if (nextButton?.text().includes('Next')) {
+    const nextURL = nextButton.attr('href');
+    if (nextURL) {
+      const nextCounts = await _countStarsFromURL(nextURL);
+      return currentCounts + nextCounts;
+    }
+  }
+  return currentCounts;
+}
+
+export default async function getStars(userID: string): Promise<number> {
+  return await _countStarsFromURL(`https://github.com/${userID}?tab=repositories`);
+};
