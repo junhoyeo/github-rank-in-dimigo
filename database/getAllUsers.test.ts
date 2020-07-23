56b5a3cf10421c63a1aab38aaefef32194f3ed7e
JunhoYeo <hanaro0704@gmail.com>
2020-07-23 17:28:45 +0900

    test(database/getAllUsers): change to expect only isArray

--- database/getAllUsers.test.ts
+++ database/getAllUsers.test.ts
@@ -1,8 +1,9 @@
 import getAllUsers from './getAllUsers';
+import { IUser } from '../models/User';
 
 describe('Get all users from database', () => {
   test('Query all users', () => {
-    const users = getAllUsers();
-    expect(users).toEqual([]);
+    const users: IUser[] = getAllUsers();
+    expect(Array.isArray(users));
   });
 });
