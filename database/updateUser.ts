29d4f0e078dc272ba4acb6ddee9c7106453a106b
Junho Yeo <hanaro0704@gmail.com>
2021-01-08 15:18:46 +0900

    feat: show star changes

--- database/updateUser.ts
+++ database/updateUser.ts
@@ -1,19 +1,21 @@
-import database from '.';
-import { IUser } from '../models/User';
-import getUserByID from './getUserByID';
+import database from ".";
+import { IUser } from "../models/User";
+import getUserByID from "./getUserByID";
 
 export default function updateUser(user: IUser): void {
   const { id: userID } = user;
 
-  if (getUserByID(userID)) {
-    database.get('users')
+  const previousUserData = getUserByID(userID);
+
+  if (previousUserData) {
+    const changes = user.stars - previousUserData.stars;
+    database
+      .get("users")
       .find({ id: userID })
-      .assign(user)
-      .write()
+      .assign({ ...user, changes })
+      .write();
     return;
   }
 
-  database.get('users')
-    .push(user)
-    .write();
+  database.get("users").push(user).write();
 }
